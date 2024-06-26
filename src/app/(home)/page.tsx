947a178669405693271d929c107911bb363f9570
Diego Gaspar <diegogaspardacruz1@gmail.com>
2024-06-26 12:01:21 -0300

    feat: add welcome message

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,17 @@
+import { format } from "date-fns";
+import { ptBR } from "date-fns/locale";
+
 export default function Home() {
   return (
-    <main>
-      Home
-    </main>
+    <div>
+      <header className="px-5 pt-5">
+        <h2 className="text-xl font-bold">Olá, Diego!</h2>
+        <p className="capitalize text-sm">
+          {format(new Date(), "EEEE, d 'de' MMMM", {
+            locale: ptBR
+          })}
+        </p>
+      </header>
+    </div>
   );
 }
