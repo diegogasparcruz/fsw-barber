cafffc3faf2321b8887741e2d46189e2535fee00
Diego Gaspar <diegogaspardacruz1@gmail.com>
2024-07-06 11:45:53 -0300

    style: ensure footer is always on the bottom

--- src/components/core/footer.tsx
+++ src/components/core/footer.tsx
@@ -1,7 +1,7 @@
 
 export const Footer = () => {
   return ( 
-    <footer className="fixed bottom-0 w-full bg-secondary py-6 px-5">
+    <footer className="w-full bg-secondary py-6 px-5">
       <p className="text-gray-400 text-xs font-bold opacity-75">
         @ 2023 Copyright FWS Barber
       </p>
