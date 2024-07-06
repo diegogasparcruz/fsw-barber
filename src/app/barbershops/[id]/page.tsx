426deb619d479450d1bfa737432e855e9b25789b
Diego Gaspar <diegogaspardacruz1@gmail.com>
2024-07-06 12:00:45 -0300

    style: update padding bottom barbershop page

--- src/app/barbershops/[id]/page.tsx
+++ src/app/barbershops/[id]/page.tsx
@@ -37,7 +37,7 @@ const BarbershopDetails = async ({ params }: BarbershopDetailsProps) => {
    <div>
     <BarbershopInfo barbershop={barbershop} />
 
-    <div className="px-5 pt-6 flex flex-col gap-4">
+    <div className="px-5 py-6 flex flex-col gap-4">
       {barbershop.services.map(service => (
         <ServiceItem 
           key={service.id} 
